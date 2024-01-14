3208b7b8625bdf225d9e9d4fddcbe515fd4cbdf9
lrpalmer27 <lrpalmer@mun.ca>
2024-01-14 12:17:53 -0330

    make all three plots at once for simplicity

--- MakeMP4.py
+++ MakeMP4.py
@@ -3,10 +3,11 @@ import tecplot as tp
 from tecplot.constant import PlotType
 troubleshooting=1
 
-OutputFilesFolder=r"C:\Users\logan\Desktop\Capstone\Converge\WorkingFiles\DET_Converge_LP\output"
-PlotVariable="PRESSURE"
-# PlotVariable="TEMPERATURE"
-# PlotVariable="MACH"
+OutputFilesFolder=r"C:\Users\logan\Desktop\Capstone\Converge\WorkingFiles\DET_Converge_LP\output_files\outputV0"
+# PlotVariable="PRESSURE"
+# # PlotVariable="TEMPERATURE"
+# # PlotVariable="MACH"
+PlotVariables=["PRESSURE","TEMPERATURE","MACH"]
 
 f=os.listdir(OutputFilesFolder)
 datafilenames=[os.path.join(OutputFilesFolder,file) for file in f]
@@ -20,12 +21,13 @@ dataset=tp.data.load_converge_hdf5(datafilenames)
 frame = tp.active_frame()
 plot = frame.plot(PlotType.Cartesian2D)
 
-plot.contour(0).colormap_name = 'Small Rainbow'
-plot.contour(0).variable = dataset.variable(PlotVariable)
+for PlotVariable in PlotVariables:
+    plot.contour(0).colormap_name = 'Small Rainbow'
+    plot.contour(0).variable = dataset.variable(PlotVariable)
 
-plot.activate()
-plot.show_contour = True
+    plot.activate()
+    plot.show_contour = True
 
-saveto=os.path.join(os.path.dirname(OutputFilesFolder),f"{PlotVariable}_Plot.mp4")
-tp.export.save_time_animation_mpeg4(saveto)
+    saveto=os.path.join(os.path.dirname(OutputFilesFolder),f"{PlotVariable}_Plot.mp4")
+    tp.export.save_time_animation_mpeg4(saveto)
     
